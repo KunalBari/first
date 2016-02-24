ca9eeb161578e682ee9dfa1df0f94009d69025c3
KunalBari <kunal.cse12@gmail.com>
2016-02-24 21:48:54 +0530

    Added files via upload
    Hello

---
+++ prog001.c
@@ -0,0 +1,24 @@
+/*
+Author: KUNAL
+Date: 8 Feb,13
+*/
+
+
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	clrscr();
+
+	printf("\n\n\tHello!");
+
+	getch();
+}
+
+
+/*
+Output:
+
+	Hello!
+*/
